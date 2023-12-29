087bfd8635ac724cfcc7cd76b78c2b0e3e643918
Sourav Malviya <85546718+souravmalviya379@users.noreply.github.com>
2023-12-29 16:43:47 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <div className="absolute top-1/2 left-1/3 h-screen w-screen">
+        <h1 className="text-orange-700 text-4xl font-bold m-2">
+          Welcome to SocialGram
+        </h1>
+      </div>
+    </>
+  )
+}
